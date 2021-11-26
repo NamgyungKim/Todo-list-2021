006ee4a0c78fa923be087d36d9d8565bb20247c6
NamgyungKim <namgyung.kim@gmail.com>
2021-11-26 17:44:30 +0900

    onKeyPress를 이용해 add_list()함수실행 및, input의 value값 지우기

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -5,17 +5,23 @@ const Input = (props) => {
   const list = props.list
 
   const add_list = () => {
-    console.log(input_vlue.current.value)
     props.setList(list.concat(input_vlue.current.value))
   }
 
+  const onKeyPress = (e) => {
+    if(e.key === 'Enter'){
+      add_list()
+      input_vlue.current.value = ''
+    }
+  }
+
   return(
-    <form className="form">
-      <input ref={input_vlue} type="text" className="input" placeholder="할일을 입력해주세요."/>
+    <div className="form">
+      <input ref={input_vlue} onKeyPress={onKeyPress} type="text" className="input" placeholder="할일을 입력해주세요."/>
       <button type="button" onClick={add_list} className="add_btn">
         <i className="fas fa-plus"></i>
       </button>
-    </form>
+    </div>
   )
 }
 
