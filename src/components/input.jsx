bbf89ddf678ee86c400c82d07989ef427f2c442e
NamgyungKim <namgyung.kim@gmail.com>
2021-11-26 16:58:54 +0900

    useRef를 이용해 input의 value값 콘솔로 나타내기

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 const Input = (props) => {
+  const input_vlue = useRef()
+  const add_list = () => {
+    console.log(input_vlue.current.value)
+  }
+
   return(
     <form className="form">
-      <input type="text" className="input"/>
-      <button className="add_btn"><i className="fas fa-plus"></i></button>
+      <input ref={input_vlue} type="text" className="input" placeholder="할일을 입력해주세요."/>
+      <button type="button" onClick={add_list} className="add_btn">
+        <i className="fas fa-plus"></i>
+      </button>
     </form>
   )
 }
